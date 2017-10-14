ca9cb7ef08f8457577b0e59f19b3e4f5a7c273d4
Jonathan Panning <jpann@users.noreply.github.com>
2017-10-13 18:34:21 -0600

    Code refactoring to improve performance
    Refactored the code by moving the contents of full30.py into __init__.py so it can take advantage of HTTP request caching. Also reworked the way video resolution is detected.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,11 @@
-from full30 import Full30
+import urllib2
+from bs4 import BeautifulSoup
+import requests
+import ssl
+import re
+import json
 
-VERSION = ' V1.0.3'
+VERSION = ' V1.0.4'
 NAME = 'Unofficial Full30.com Plex Channel'
 TITLE = 'Unofficial Full30.com Plex Channel'
 ART   = 'art-default.jpg'
@@ -10,8 +15,11 @@ HTTP_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/53
 
 ROUTE = "/video/fullthirty"
 BASE_URL = "https://www.full30.com"
-
-full30 = Full30(BASE_URL)
+CHANNELS_URL = BASE_URL + "/channels/all"
+VIDEO_URL = BASE_URL + "/video/{0}"
+RECENT_API_URL = BASE_URL + "/api/v1.0/channel/{0}/recent-videos?page={1}"
+THUMBNAIL_URL = BASE_URL + "/cdn/videos/{0}/{1}/thumbnails/320x180_{2}.jpg"
+VIDEO_RESOLUTIONS_URL = "https://videos.full30.com/bitmotive/public/full30/v1.0/videos/{0}/{1}"
 
 def Start():
     ObjectContainer.title1 = TITLE
@@ -29,7 +37,7 @@ def Start():
 def MainMenu():
     oc = ObjectContainer()
     
-    channels = full30.get_channels()
+    channels = get_channels()
     
     for channel in channels:
         title = channel['name']
@@ -85,7 +93,7 @@ def Channel_Menu(title, channel_url, thumbnail):
 def Channel_Recent(title, channel_url, page=1):
     oc = ObjectContainer(title2 = title)
 
-    recent_videos = full30.get_recent_by_page(channel_url, page)
+    recent_videos = get_recent(channel_url, page)
     
     limit = recent_videos['pages']
 
@@ -93,9 +101,10 @@ def Channel_Recent(title, channel_url, page=1):
         url = video['url']
         title = video['title']
         thumb = video['thumbnail']
+        mp4_url = video['mp4_url']
         
         oc.add(VideoClipObject(
-            url = url,
+            url = mp4_url,
             title = title,
             summary = title,
             thumb = Callback(Thumb, url=thumb)
@@ -114,15 +123,14 @@ def Channel_Recent(title, channel_url, page=1):
             title = "Page {0}".format(next_page),
             summary = "View more videos"
        ))
-            
-        
+
     return oc
 
 @route(ROUTE + "/Featured")
 def Channel_Featured(title, channel_url):
     oc = ObjectContainer(title2 = title)
 
-    featured_videos = full30.get_featured(channel_url)
+    featured_videos = get_featured(channel_url)
 
     Log.Info(featured_videos)
 
@@ -130,9 +138,10 @@ def Channel_Featured(title, channel_url):
         url = video['url']
         title = video['title']
         thumb = video['thumbnail']
+        mp4_url = video['mp4_url']
 
         oc.add(VideoClipObject(
-            url = url,
+            url = mp4_url,
             title = title,
             summary = title,
             thumb = Callback(Thumb, url=thumb)
@@ -146,3 +155,107 @@ def Thumb(url):
     return DataObject(data, 'image/jpeg')
   except:
     return Redirect(R(ICON))
+
+def get_channels():
+    channels = []
+
+    data = HTTP.Request(CHANNELS_URL, cacheTime = CACHE_1HOUR).content
+
+    soup = BeautifulSoup(data, "html.parser")
+
+    videostreams = soup.find(class_="small-12 medium-4 large-4 columns show-for-large")
+
+    for video in videostreams.find_next('ul').find_all('li'):
+        channel_url = BASE_URL + video.find('a').get('href')
+        
+        channel_name = video.find('span', class_='channel-name text-uppercase').string.encode('ascii', 'ignore').decode('ascii')
+        channel_thumbnail = BASE_URL + video.find('img', class_="channel-image").get('src')
+
+        channels.append({ "name" : channel_name, "url" : channel_url, "thumbnail" : channel_thumbnail })
+
+    return channels
+    
+def get_featured(url):
+    featured = []
+
+    data = HTTP.Request(url, cacheTime = 1200).content
+
+    soup = BeautifulSoup(data, "html.parser")
+    
+    slug = soup.find(id="channel-slug").get('value')
+    print 'slug: ', slug
+
+    featured_videos = soup.find(class_=re.compile("featured-row"))
+    
+    if featured_videos:
+        for video in featured_videos.find_all('div', class_="recent-item"):
+            video_url = BASE_URL + video.find('a').get('href').encode('ascii', 'ignore').decode('ascii')
+            video_hash = video_url.rsplit('/', 1)[-1]
+
+            video_name = video.find('p', class_=re.compile('recent-title')).string.encode('ascii', 'ignore').decode('ascii')
+            video_thumbnail = "http:" + video.find('img', class_="recentlyAdded-pictures").get('src')
+
+            res_url = VIDEO_RESOLUTIONS_URL.format(slug, video_hash)
+            
+            video_metadata = get_video_metadata(video_url)
+
+            featured.append(
+                { 
+                    "title" : video_name, 
+                    "url" : video_url, 
+                    "thumbnail" : video_thumbnail ,
+                    "description" : video_metadata['desc'],
+                    "mp4_url" : res_url
+                })
+
+    return featured
+
+def get_recent(url, page):
+    recent = { 'title' : '', 'slug' : '', 'pages' : '', 'videos' : [] }
+    
+    if not page:
+        page = 1
+    
+    channel_name = url.rsplit('/', 1)[-1]
+    api_url = RECENT_API_URL.format(channel_name, page)
+    
+    html = HTTP.Request(api_url, cacheTime = 1200).content
+    
+    if not html:
+        return None
+        
+    data = json.loads(html)
+    
+    if not data:
+        return None
+        
+    recent['title'] = data['channel']['title']
+    recent['pages'] = data['pages']
+    recent['slug'] = data['channel']['slug']
+    
+    for video in data['videos']:
+        v_hash = video['hashed_identifier']
+        v_id = video['id']
+        v_thumbnail = THUMBNAIL_URL.format(recent['slug'], v_hash, video['thumbnail_filename'])
+        v_title = video['title']
+        v_url = VIDEO_URL.format(v_hash)
+        v_channel = recent['title']
+
+        v_res_url = VIDEO_RESOLUTIONS_URL.format(recent['slug'], v_hash)
+        
+        recent['videos'].append({ "title" : v_title, "url" : v_url, "thumbnail" : v_thumbnail, "channel": v_channel, "mp4_url" : v_res_url })
+    
+    return recent
+
+def get_video_metadata(url):
+    metadata = {}
+    
+    data = HTTP.Request(url, cacheTime = CACHE_1MONTH).content
+
+    soup = BeautifulSoup(data, "html.parser")
+    
+    metadata['desc'] = soup.find('meta', property='og:description').get('content')
+    #metadata['uploaded'] = date
+    metadata['views'] = soup.find('span', id="player-view-count").text
+    
+    return metadata
